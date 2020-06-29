d1d2e24b7a91d3850e0edcc3102e66986799cb3b
msJavaCoder <271457837@qq.com>
2020-06-29 18:10:44 +0800

    初始化提交    1.完成SpringBoot项目搭建    2.完成用户登录功能及拦截器    3.完成用户名和用户昵称修改功能   缺陷：MD5加密有问题. 暂时不能修改密码

---
+++ src/main/java/com/imood/blog/service/impl/AdminUserServiceImpl.java
@@ -0,0 +1,80 @@
+package com.imood.blog.service.impl;
+
+import com.imood.blog.entity.AdminUser;
+import com.imood.blog.mapper.AdminUserMapper;
+import com.imood.blog.service.AdminUserService;
+import com.imood.blog.util.MD5Util;
+import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
+
+import javax.annotation.Resource;
+import java.nio.charset.StandardCharsets;
+
+@Service
+public class AdminUserServiceImpl implements AdminUserService {
+
+    @Resource
+    private AdminUserMapper adminUserMapper;
+
+    @Override
+    public AdminUser login(String userName, String password) {
+
+        /*AdminUser adminUser = adminUserMapper.selectByUsername(userName);
+        if(adminUser==null){
+            throw new RuntimeException("用户名或密码错误");
+        }*/
+      /*  String passwordMd5 = MD5Util.MD5Encode(password, "UTF-8");*/
+        /*if (!password.equalsIgnoreCase(
+                DigestUtils.md5DigestAsHex(password.getBytes(StandardCharsets.UTF_8)))) {
+            throw new RuntimeException("用户名或密码错误");
+        }*/
+        return adminUserMapper.login(userName, password);
+    }
+
+
+    @Override
+    public AdminUser getUserDetailById(Integer loginUserId) {
+        return adminUserMapper.selectByPrimaryKey(loginUserId);
+    }
+
+    @Override
+    public Boolean updatePassword(Integer loginUserId, String originalPassword, String newPassword) {
+        AdminUser adminUser = adminUserMapper.selectByPrimaryKey(loginUserId);
+        //当前用户非空才可以进行更改
+        if (adminUser != null) {
+            String originalPasswordMd5 = MD5Util.MD5Encode(originalPassword, "UTF-8");
+            String newPasswordMd5 = MD5Util.MD5Encode(newPassword, "UTF-8");
+            //比较原密码是否正确
+            if (originalPasswordMd5.equals(adminUser.getLoginPassword())) {
+                //设置新密码并修改
+                adminUser.setLoginPassword(newPasswordMd5);
+                if (adminUserMapper.updateByPrimaryKeySelective(adminUser) > 0) {
+                    //修改成功则返回true
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Boolean updateName(Integer loginUserId, String loginUserName, String nickName) {
+        AdminUser adminUser = adminUserMapper.selectByPrimaryKey(loginUserId);
+        //当前用户非空才可以进行更改
+        if (adminUser != null) {
+            //设置新密码并修改
+            adminUser.setLoginUserName(loginUserName);
+            adminUser.setNickName(nickName);
+            if (adminUserMapper.updateByPrimaryKeySelective(adminUser) > 0) {
+                //修改成功则返回true
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public AdminUser selectByUsername(String userName) {
+        return adminUserMapper.selectByUsername(userName);
+    }
+}
---
+++ src/test/java/com/imood/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imood.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
